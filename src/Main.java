3327d61a9f9158b99f7e93ac15dfa24e4b65c8ca
asuravadha <rr.easwar@gmail.com>
2021-11-10 17:50:39 +0530

    sample commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void  main(String[] args){
-        Scanner input=new Scanner(System.in);
-        System.out.println("enter your name");
-        String name=input.next();
 
-        System.out.println("\n hi "+name+" welcome to dubai");
+
+
+
+//        Scanner input=new Scanner(System.in);
+//        System.out.println("control statements");
+//        System.out.println("enter your value");
+//
+//        int value=input.nextInt();
+//
+//        if(value>0){
+//            System.out.println("it is positive");
+//        }
+//        else if(value==0){
+//            System.out.println("it is zero");
+//        }
+//        else{
+//            System.out.println("it is -ve");
+//        }
+
+
+
+
+//        float a=13,b=2;
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//        System.out.println(a%b);
+
+
+
+
+//        Scanner input=new Scanner(System.in);
+//
+//        String name=input.next(); //string value
+//        int a=input.nextInt();
+//        System.out.println(name+" you entered a value as "+a);
+
+
+//        Scanner input=new Scanner(System.in);
+//        System.out.println("enter your name");
+//        String name=input.next();
+//        System.out.println("\n hi "+name+" welcome to dubai");
 
 
 
